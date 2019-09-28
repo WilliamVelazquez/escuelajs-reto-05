3a2aa52caddea2ce6aa4db0f29084981dc632e80
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-09-28 12:26:50 -0500

    Solving 1st and 2nd Problem
    Saving url on Local Storage with the key next_fetch.
    Getting data from the next url.
    Updating code to use Async/Await.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+// const API = 'https://rickandmortyapi.com/api/character/';
+// const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+const API = 'https://swapi.co/api/people';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
+const getData = async api => {
+  try {
+    const request = await fetch(api);
+    const data = await request.json();
+  
+    const characters = data.results;
+    let output = characters.map(character => {
+      // <img src="${character.image}" />
+    return `
+    <article class="Card">
+      <h2>${character.name}<span>${character.eye_color}</span></h2>
+    </article>
     `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+    }).join('');
+    let newItem = document.createElement('section');
+    newItem.classList.add('Items');
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+    localStorage.setItem('next_fetch', data.next);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const loadData = () => {
-  getData(API);
+  const storageURL = localStorage.getItem('next_fetch');
+  console.log('storageURL--->',storageURL);
+  
+  const URL = storageURL?storageURL:API;
+  console.log('URL',URL);
+
+  (!URL||URL==='null')?console.log('Fin'):getData(URL);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
