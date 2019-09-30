08f7733a9c70c7bfd6aba9dd8b98f8e20d9ab600
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-09-29 19:58:46 -0500

    Switching Back to the Rick and Morty API.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-// const API = 'https://rickandmortyapi.com/api/character/';
+const API = 'https://rickandmortyapi.com/api/character/';
 // const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
-const API = 'https://swapi.co/api/people';
+// const API = 'https://swapi.co/api/people';
 
 localStorage.removeItem('next_fetch');
 
 const getData = async api => {
   try {
+    intersectionObserver.unobserve($observe);
     const request = await fetch(api);
     const data = await request.json();
   
     const characters = data.results;
     let output = characters.map(character => {
-      // <img src="${character.image}" />
-    return `
-    <article class="Card">
-      <h2>${character.name}<span>${character.eye_color}</span></h2>
-    </article>
-    `
+      return `
+        <article class="Card">
+          <img src="${character.image}" />
+          <h2>${character.name}<span>${character.species}</span></h2>
+        </article>
+      `
     }).join('');
     let newItem = document.createElement('section');
     newItem.classList.add('Items');
     newItem.innerHTML = output;
     $app.appendChild(newItem);
-    localStorage.setItem('next_fetch', data.next);
+    localStorage.setItem('next_fetch', data.info.next);
+    // console.log('loaded :)');
+    intersectionObserver.observe($observe);
   } catch (error) {
     console.log(error);
   }
@@ -47,10 +50,10 @@ const loadData = () => {
   const storageURL = localStorage.getItem('next_fetch');
   // console.log('storageURL--->',storageURL);
   
-  const URL = storageURL?storageURL:API;
+  const URL = storageURL?storageURL:(storageURL===null?API:'');
   // console.log('URL',URL);
 
-  (!URL||URL==='null')?end():getData(URL);
+  !URL?end():getData(URL);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
